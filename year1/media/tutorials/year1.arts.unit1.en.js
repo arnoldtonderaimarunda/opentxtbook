cfa1f3ec2842a4518db88cd0735d454bcba7e2d5
arnoldtonderaimarunda <hello@workflowautomation.expert>
2024-09-14 13:18:45 +0200

    God is gracious and kind. Glory be to him

--- year1/media/tutorials/year1.arts.unit1.en.js
+++ year1/media/tutorials/year1.arts.unit1.en.js
@@ -67,19 +67,23 @@ undum.game.situations = {
     // more sense than writing it longhand.
     module1: new undum.Situation({
         enter: function(character, system, from) {
+            system.setQuality("endurance", character.qualities.endurance+1);
             system.write($("#s_module1").html());
         },
         tags: ["module"],
-        optionText: "Module 1 - Emergent Reading and Writing Skills",
+        optionText: "MODULE 1 - Emergent Reading and Writing Skills",
         displayOrder: 1
     }),
-    todo: new undum.SimpleSituation(
-        "<p><a href='/media/tutorials/' class='sticky'>Download Worksheet 1.1</a>and complete the required assignment. <a href='./worksheet1.1.pdf'>Done assignment!</a>.\
-        <p class='transient'>Make sure student has carried out the action above. Including clicking [Done assignment!],\
-        then <a href='hub'>return to the module list</a>.</p>",
+    todo1: new undum.SimpleSituation(
+        "<p><a href='/media/tutorials/worksheet1.1.pdf' class='sticky'>Download worksheet 1.1</a>.</p>",
         {
-            actions: {
-                'worksheet1-1': "<p>You carried out Take Home Worksheet 1.1. Well done!</p>"
+        enter: function(character, system, from) {
+	    system.writeChoices(system.getSituationIdChoices(['#attitude']));
+        },
+
+            exit: function(character, system, to) {
+            	system.setQuality("skill", character.qualities.skill+1);
+		system.setQuality("novice", 1);
             }
         }
     ),
@@ -88,141 +92,19 @@ undum.game.situations = {
             system.write($("#s_module2").html());
         },
         tags: ["module"],
-        optionText: "Module 2 - Emergent Reading and Writing Skills",
+        optionText: "MODULE 2 - Emergent Reading and Writing Skills",
         displayOrder: 2
     }),
-    sticky: new undum.SimpleSituation(
-        "<p>There are three types of link in Undum. The first two we've seen\
-        in previous modules:\
-        links to change situation and links to carry out an action. When you\
-        include a link in your output, Undum parses it and wires it up\
-        correctly. If you create a link with a HTML <em>href</em> attribute\
-        containing just a name ('ballroom', for\
-        example) this will send the character to the situation with that\
-        name. Links\
-        with two components ('ballroom/view-painting', for example) send\
-        the character to a new situation <em>and then</em> carry out the\
-        named action ('view-painting' in this case). To carry out an action\
-        in the current situation, you can replace the situation name with a\
-        dot (so it would be './view-painting'). In all cases, if the\
-        character is already in that situation, then the situation's\
-        <em>enter</em> method won't be called again.</p>\
-        \
-        <img src='media/games/tutorial/woodcut2.png' class='float_left'>\
-        <p>The third type of link, then, is a general hyperlink. If your\
-        link doesn't consist of a single element or pair of elements, as\
-        above, then Undum will guess that you have a normal hyperlink. As\
-        <a href='http://news.bbc.co.uk' class='sticky'>in this link</a>.\
-        If you have a link that <em>does</em> look like an Undum link, you\
-        can still force Undum not to interpret it as an action or situation\
-        move, by adding the CSS class <em>raw</em> to the HTML <em>a</em> tag.\
-        links that don't have the <em>raw</em> class, but that are considered\
-        to be non-Undum links (like the link above), will have <em>raw</em>\
-        added to them before display. This could allow you to style external\
-        links differently, as we have done here.</p>\
-        \
-        <p>In the last situation I said you can prevent links from being\
-        turned into regular text when you move situations. This is done\
-        by another CSS class: <em>sticky</em>. When you\
-        <a href='oneshot'>leave this situation</a>, you'll notice the\
-        external link stays active. This can allow you to have options that\
-        stay valid throughout the narrative, for example, such as a spell to\
-        teleport home.</p>",
+   todo2: new undum.SimpleSituation(
+        "<p><a href='/media/tutorials/worksheet2.1.pdf' class='sticky'>Download worksheet 2.1</a>.</p>",
         {
-            tags: ["module"],
-            displayOrder: 3,
-            heading: "Different Kinds of Links"
-        }
-    ),
-    oneshot: new undum.SimpleSituation(
-        "<p>There is one final option for links. If you give a link\
-        the <em>once</em> CSS class, then that link will disappear\
-        after it is clicked. This is  used (as in\
-        <a href='./one-time-action' class='once'>this link</a>) for\
-        actions that you only want to be possible once. There is no\
-        point using 'once' on situation links because they'll be turned\
-        into text as soon as you click them anyway (unless they are also\
-        <em>sticky</em>, of course).</p><p>Once links are useful\
-        for actions such as examining an object more carefully. You\
-        don't want lots of repeated descriptions, so making the link\
-        a <em>once</em> link is more user friendly.</p>\
-        <p>If you have more than one link to the same action, then all\
-        matching links will be removed, so you don't have to worry about\
-        the player having an alternative way to carry out the action.</p>\
-        <p class='transient'>After you've clicked the link, let's\
-        <a href='hub'>move on</a>.</p>",
-        {
-            actions: {
-                "one-time-action": "<p>As I said, one time actions are\
-                                   mostly used to describe something in\
-                                   more detail, where you don't want the\
-                                   same descriptive text repeated over and\
-                                   over</p>"
-            }
-        }
-    ),
-    qualities: new undum.SimpleSituation(
-        "<p>Let's talk about the character.\
-        The character is described by a series of <em>qualities</em>. These\
-        are numeric values that can describe anything from natural abilities\
-        to how much of a resource the character controls. Qualities are\
-        shown in the box on the right of the text.</p>\
-        \
-        <p>The qualities there are those you started the game with. When you\
-        <a href='quality-types'>go to the next situation</a>, keep your\
-        eyes on the character panel. You'll notice I'll give you a boost to\
-        your stamina quality. This process is animated and highlighted to\
-        draw your attention to it. You could also get a boost of skill\
-        by carrying out <a href='./skill-boost'>this action</a> as many\
-        times as you like.</p>",
-        {
-            heading: "Qualities and the Character",
-            tags: ["module"],
-            displayOrder: 4,
-            actions: {
-                "skill-boost": function(character, system, action) {
-                    system.setQuality("skill", character.qualities.skill+1);
-                }
-            },
+        enter: function(character, system, from) {
+	    system.writeChoices(system.getSituationIdChoices(['#attitude']));
+        },
+
             exit: function(character, system, to) {
-                system.setQuality("stamina", character.qualities.stamina+1);
-            }
-        }
-    ),
-    "quality-types": new undum.SimpleSituation(
-        "<p>Not all the qualities in the character panel are displayed as\
-        numeric. Internally they are all numeric, but different qualities\
-        get to choose how to display themselves. So 'Luck', for example, is\
-        displayed as words (based on the FUDGE RPG's adjective scale),\
-        and 'Novice' is using just a check-mark.</p>\
-        \
-        <p>To see how Luck changes, try using this\
-        <a href='./luck-boost'>luck-boosting action</a> or this\
-        <a href='./luck-reduce'>luck-reducing action</a>. Notice that\
-        luck uses a numeric bonus when it runs out of words. There are a range\
-        of different display types provided with Undum, and you can easily\
-        add your own too.</p>\
-        \
-        <p>When you <a href='character-text'>leave this situation</a>,\
-        I'll set 'Novice' to zero. Watch\
-        the character panel, and you'll see that Novice decides it doesn't\
-        need to be displayed any more and will be removed. You will also see\
-        that when the last\
-        quality in a group is removed ('Novice' is in the 'Progress' group),\
-        then the group heading is also removed. You can tell Undum what\
-        group each quality belongs to, and what order they should be listed.\
-        <p>",
-        {
-            actions: {
-                "luck-boost": function(character, system, action) {
-                    system.setQuality("luck", character.qualities.luck+1);
-                },
-                "luck-reduce": function(character, system, action) {
-                    system.setQuality("luck", character.qualities.luck-1);
-                }
-            },
-            exit: function(character, system, to) {
-                system.setQuality("novice", 0);
+            	system.setQuality("skill", character.qualities.skill+1);
+		system.setQuality("novice", 1);
             }
         }
     ),
@@ -247,122 +129,53 @@ undum.game.situations = {
             }
         }
     ),
-    progress: new undum.SimpleSituation(
-        "<p>Sometimes you want to make the change in a quality into a more\
-        significant event. You can do this by animating the change in\
-        quality. If you <a href='./boost-stamina-action'>boost your\
-        stamina</a>, you will see the stamina change in the normal\
-        way in the character panel. But you will also see a progress\
-        bar appear and animate below.</p>",
-        {
-            tags: ["module"],
-            heading: "Showing a Progress Bar",
-            displayOrder: 5,
-            actions: {
-                // I'm going indirect here - the link carries out an
-                // action, which then uses doLink to directly change
-                // the situation.  This isn't the recommended way (I
-                // could have just changed situation in the link), but
-                // it illustrates the use of doLink.
-                "boost-stamina-action": function(character, system, action) {
-                    system.doLink("boost-stamina");
-                }
-            },
-            exit: function(character, system, to) {
-                system.animateQuality(
-                    'stamina', character.qualities.stamina+1
-                );
-            }
-        }
-    ),
-    "boost-stamina": new undum.SimpleSituation(
-        "<p>\
-        <img src='media/games/tutorial/woodcut3.png' class='float_right'>\
-        The progress bar is also useful in situations where the\
-        character block is displaying just the whole number of a quality,\
-        whereas some action changes a fraction. If the quality is displaying\
-        the character's level, for example, you might want to show a progress\
-        bar to indicate how near the character is to levelling up.</p>\
-        \
-        <p>After a few seconds, the progress bar disappears, to keep the\
-        focus on the text. Undum isn't designed for games where a lot of\
-        statistic management is needed. If you want a change to be part\
-        of the permanent record of the game, then write it in text.</p>\
-        \
-        <p>Let's <a href='hub'>return to the module list.</a></p>"
-        ),
-    // Again, we'll retrieve the text we want from the HTML file.
-    "saving": new undum.Situation({
-        enter: function(character, system, from) {
-            system.write($("#s_saving").html());
-        },
-        tags: ["module"],
-        displayOrder: 6,
-        optionText: "Saving and Loading"
-    }),
-
     "implicit-boost": new undum.SimpleSituation(
-        "<p>Your luck has been boosted<span class='transient'>, check the\
-        list of options to see if they have changed</span>.</p>",
+        "<p>Students attitude has been boosted<span class='transient'></span>.</p><p class='transient'><a href='hub'>Return to the module list...</a>.</p>",
         {
-            tags: ["example"],
+            tags: ["attitude"],
             enter: function(character, system, from) {
-                system.animateQuality("luck", character.qualities.luck+1)
-                system.doLink('example-choices');
+                system.animateQuality("attitude", character.qualities.attitude+1)
             },
-            optionText: "Boost Your Luck",
+            optionText: "Boost students attitude",
             displayOrder: 1,
             canView: function(character, system, host) {
-                return character.qualities.luck < 4;
+                return character.qualities.attitude < 4;
             }
         }
     ),
     "implicit-drop": new undum.SimpleSituation(
-        "<p>Your luck has been reduced<span class='transient'>, check the\
-        list of options to see if they have changed</span>.</p>",
+        "<p>Students attitude has been reduced<span class='transient'></span>.</p><p class='transient'><a href='hub'>Return to the module list...</a>.</p>",
         {
-            tags: ["example"],
+            tags: ["attitude"],
             enter: function(character, system, from) {
-                system.animateQuality("luck", character.qualities.luck-1)
-                system.doLink('example-choices');
+                system.animateQuality("attitude", character.qualities.attitude-1)
             },
-            optionText: "Reduce Your Luck",
+            optionText: "Reduce students attitude",
             displayOrder: 2,
             canView: function(character, system, host) {
-                return character.qualities.luck > -4;
+                return character.qualities.attitude > -4;
             }
         }
     ),
-    "high-luck-only": new undum.SimpleSituation(
-        "<p>Your luck is higher than 'fair'. The link to this \
-        situation would not\
-        have appeared if it were lower.</p>",
+    "high-attitude-only": new undum.SimpleSituation(
+        "<p>Students attitude is higher than 'fair'.</p><p class='transient'><a href='hub'>Return to the module list...</a>.</p>",
         {
-            tags: ["example"],
-            enter: function(character, system, from) {
-                system.doLink('example-choices');
-            },
-            optionText: "High Luck Option",
+            tags: ["attitude"],
+            optionText: "High attitude",
             displayOrder: 3,
             canView: function(character, system, host) {
-                return character.qualities.luck > 0;
+                return character.qualities.attitude > 0;
             }
         }
     ),
-    "low-luck-only": new undum.SimpleSituation(
-        "<p>Your luck is lower than 'fair'. The link to this situation \
-        appears whether\
-        it is low or high, but can only be chosen if it is low. It does this\
-        by defining a <em>canChoose</em> method.</p>",
+    "low-attitude-only": new undum.SimpleSituation(
+        "<p>Students attitude is lower than 'fair'.</p><p class='transient'><a href='hub'>Return to the module list...</a>.</p>",
         {
-            tags: ["example"],
-            enter: function(character, system, from) {
-                system.doLink('example-choices');
-            },
-            optionText: "Low Luck Option (requires low luck to be clickable)",
+            tags: ["attitude"],
+            optionText: "Low attitude",
             displayOrder: 3,
             canChoose: function(character, system, host) {
-                return character.qualities.luck < 0;
+                return character.qualities.attitude < 0;
             }
         }
     ),
@@ -380,7 +193,7 @@ undum.game.situations = {
         <h1>The End</h1>",
         {
             tags: ["module"],
-            optionText: "Finish the Tutorial",
+            optionText: "Finish the Unit",
             displayOrder: 8,
             enter: function(character, system, from) {
                 system.setQuality("inspiration", 1);
@@ -404,11 +217,11 @@ undum.game.qualities = {
     skill: new undum.IntegerQuality(
         "Skill", {priority:"0001", group:'stats'}
     ),
-    stamina: new undum.NumericQuality(
-        "Stamina", {priority:"0002", group:'stats'}
+    endurance: new undum.NumericQuality(
+        "Endurance", {priority:"0002", group:'stats'}
     ),
-    luck: new undum.FudgeAdjectivesQuality( // Fudge as in the FUDGE RPG
-        "<span title='Skill, Stamina and Luck are reverently borrowed from the Fighting Fantasy series of gamebooks. The words representing Luck are from the FUDGE RPG. This tooltip is illustrating that you can use any HTML in the label for a quality (in this case a span containing a title attribute).'>Luck</span>",
+    attitude: new undum.FudgeAdjectivesQuality( // Fudge as in the FUDGE RPG
+        "<span title='Skill, Endurance and Attitude are reverently borrowed from the Fighting Fantasy series of gamebooks. The words representing attitude are from the FUDGE RPG. This tooltip is illustrating that you can use any HTML in the label for a quality (in this case a span containing a title attribute).'>Attitude</span>",
         {priority:"0003", group:'stats'}
     ),
 
@@ -435,10 +248,10 @@ undum.game.qualityGroups = {
 /* This function gets run before the game begins. It is normally used
  * to configure the character at the start of play. */
 undum.game.init = function(character, system) {
-    character.qualities.skill = 12;
-    character.qualities.stamina = 12;
-    character.qualities.luck = 0;
-    character.qualities.novice = 1;
+    character.qualities.skill = 0;
+    character.qualities.endurance = 0;
+    character.qualities.attitude = 0;
+    character.qualities.novice = 5;
     character.qualities.inspiration = 0;
-    system.setCharacterText("<p>You are starting on an exciting journey.</p>");
+    system.setCharacterText("<p>Students are starting on an exciting journey.</p>");
 };
